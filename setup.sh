e4eab41cfb4261ec3fd15d953d6c29651eb84da4
Suhas R G <RGSUHAS69@GMAIL.COM>
2024-12-14 23:39:43 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
-#!/bin/bash
 sudo apt-get update
-sudo apt-get install -y zlib1g-dev libgl1-mesa-glx
+sudo apt-get install -y libjpeg-dev zlib1g-dev
+```[_{{{CITATION{{{_1{[Fixed] “Failed building wheel for Pillow” in Python - CSEStack](https://www.csestack.org/failed-building-wheel-pillow-python/)
+
