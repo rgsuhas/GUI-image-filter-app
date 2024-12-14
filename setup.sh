8c75945332c1d99c52a7ea0e94d8f1d0d17d19e2
Suhas R G <RGSUHAS69@GMAIL.COM>
2024-12-14 23:45:07 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,3 @@
-sudo apt-get update
-pip install --upgrade pip
-```[_{{{CITATION{{{_2{Installation - pip documentation v24.3.1](https://pip.pypa.io/en/stable/installation/)
-
-
-
-sudo apt-get install -y libjpeg-dev zlib1g-dev
-```[_{{{CITATION{{{_1{[Fixed] “Failed building wheel for Pillow” in Python - CSEStack](https://www.csestack.org/failed-building-wheel-pillow-python/)
-
+sudo apt-get install libjpeg-dev zlib1g-dev
+pip3 install Pillow
+sudo pip install -U setuptools
